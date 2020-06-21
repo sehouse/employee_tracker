b18b960cd4f45d2186036c2430ac7ed0f8ddebe3
sehouse <sehouse@umich.edu>
2020-06-21 14:45:32 -0400

    Addition of switch/case for selection of option from main menu and reorganization of options.

--- server.js
+++ server.js
@@ -19,19 +19,39 @@ connection.connect((error) => {
     beginProgram();
 });
 
+//Function to begin the Employee Tracker
 const beginProgram = () => {
     inquirer.prompt({
         name: 'action',
         type: 'list',
-        message: 'Welcome to the TJPW Employee Database! Please select an option from the following choices:',
+        message: 'Welcome to the TNJPW Employee Database! Please select an option from the following choices:',
         choices: [
             'View all employees',
-            'View all departments.',
-            'View all roles',
             'Add an employee',
+            'View all departments',
             'Add a department',
+            'View all roles',
             'Add a role',
             'Close the application'
         ]
+    }).then(() => {
+        switch (answer.action) {
+            case 'View all employees':
+                break;
+            case 'Add an employee':
+                break;
+            case 'View all departments':
+                break;
+            case 'Add a department':
+                break;
+            case 'View all roles':
+                break;
+            case 'Add a role':
+                break;
+            case 'Close the application':
+                break;
+            default:
+                break;
+        }
     })
 }
